b17b1a566e3b703f39330205b8ee53ae6c04bdb1
784056810 <784056810@qq.com>
2024-03-28 17:05:54 +0800

    feat:初始化提交

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,15 @@
+import { iconUrl } from "./sl_icon/config";
+export const iconConfig = iconUrl;
+
+// 导出组件
 export { default as UploadOss } from "./sl_upload_oss";
+export { default as Icon } from "./sl_icon";
+
+// 导出类型
 export type {
   UploadProps,
   UploadFile,
   ResultsLists,
   FileTypes,
 } from "./sl_upload_oss";
-
-export { default as Icon } from "./sl_icon";
 export type { IconProps } from "./sl_icon";
-import { iconUrl } from "./sl_icon/config";
-export const iconConfig = iconUrl;
